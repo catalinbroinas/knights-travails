4a995500fa88d649334aa021ab616c2ab3e55da1
catalinbroinas <catalinbroinas@yahoo.com>
2024-08-08 16:38:44 +0300

    Add `KnightTravails` function  * Matrix initialization and `getBoard` method

---
+++ knightsTravails.js
@@ -0,0 +1,9 @@
+function KnightTravails() {
+    // Initialize the chessboard (8x8 matrix) with null
+    const size = 8;
+    const chessBoard = Array(size).fill(null).map(() => Array(size).fill(null));;
+
+    const getBoard = () => chessBoard;
+}
+
+export { KnightTravails };
---
+++ main.js
@@ -0,0 +1,4 @@
+import { KnightTravails } from "./knightsTravails.js";
+
+// Create an instance of KnightTravails
+const game = KnightTravails();
