14bb589b61431e60ca1f67aa2fd865f0163b86c9
catalinbroinas <catalinbroinas@yahoo.com>
2024-08-08 19:58:10 +0300

    Test `knightMoves` functionality

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ import { KnightTravails } from "./knightsTravails.js";
 // Create an instance of KnightTravails
 const game = KnightTravails();
 
-const start = [3, 3];
-const end = [4, 3];
+// Test knightMoves function
+let start = [3, 3];
+let end = [4, 3];
+game.knightMoves(start, end);
 
+start = [0, 0];
+end = [3, 3];
+game.knightMoves(start, end);
+
+start = [3, 3];
+end = [0, 0];
+game.knightMoves(start, end);
+
+start = [7, 7];
+end = [7, 7];
 game.knightMoves(start, end);
